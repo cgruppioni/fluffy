0c459cbcf24dda8de59dfcba54c100c2e0fcf680
Casey Gruppioni <cgruppioni@gmail.com>
2014-08-25 14:31:01 -0400

    Sleep counter functional

--- app/models/sleeper.rb
+++ app/models/sleeper.rb
@@ -10,14 +10,13 @@ class Sleeper
   end
 
   def sleep
+    reset_sleep_counter
     if wants_to_sleep?
       @adoption.update_attributes(
         score: @adoption.score + POINTS,
         sleep_counter: @adoption.sleep_counter.next,
         last_time_slept: Time.now
       )
-    else
-      reset_sleep_counter
     end
   end
 
@@ -26,8 +25,10 @@ class Sleeper
   end
 
   def reset_sleep_counter
-    if @adoption.last_time_slept > SLEEP_TIME.hours.ago
-      sleep_counter = 0
+    if @adoption.last_time_slept < SLEEP_TIME.hours.ago
+      @adoption.update_attributes(
+        sleep_counter: 0
+      )
     end
   end
 end
