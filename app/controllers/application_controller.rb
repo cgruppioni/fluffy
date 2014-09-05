21edb3679709541654738c2b33a928b49c25941e
Casey Gruppioni <cgruppioni@gmail.com>
2014-09-05 18:23:16 -0400

    Guest object

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   include Monban::ControllerHelpers
-  before_action :require_login
-  protect_from_forgery with: :exception
+
+  def current_user
+    super || Guest.new
+  end
 end
